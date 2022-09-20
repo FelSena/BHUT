469e9457f649f4de7bbc9e7c1057a4546c2bf46c
Felipe Sena <felipe.desena@hotmail.com>
2022-09-20 15:23:36 -0300

    XML fix

--- teste_bhut/src/components/Api/index.tsx
+++ teste_bhut/src/components/Api/index.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://api-test.bhut.com.br:3000/api/cars/",
+  baseURL: "https://api-test.bhut.com.br:3000/api/cars/",
 });
 
 export default API;
